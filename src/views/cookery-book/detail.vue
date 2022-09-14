1907a6405fb22f0487bcb39add99aef434bdd055
Cxx0822 <1556464090@qq.com>
2022-09-14 22:43:53 +0800

    更新菜谱菜单显示

--- src/views/cookery-book/detail.vue
+++ src/views/cookery-book/detail.vue
@@ -3,8 +3,10 @@
     <el-card class="box-card">
       <el-row :gutter="20">
         <el-col :span="12">
-          <h1>基础信息</h1>
-          <el-form :model="cookeryBookInfo" label-width="120px">
+          <div class="title-div">
+            <el-tag class="ml-2" type="info" size="large" round>基础信息</el-tag>
+          </div>
+          <el-form :model="cookeryBookInfo" label-width="70px">
             <el-form-item label="菜谱名称">
               <el-input v-model="cookeryBookInfo.name" />
             </el-form-item>
@@ -31,33 +33,31 @@
             <el-form-item label="材料">
               <div style="margin-bottom: 10px">
                 <el-input v-model="materialInfo.name" placeholder="名称" style="width: 100px;"/>
-                <el-input-number v-model="materialInfo.number" :min="0" :precision="2"/>
-                <el-select v-model="materialInfo.company" filterable allow-create :reserve-keyword="false" placeholder="单位" style="width: 100px;">
+                <el-input-number v-model="materialInfo.number" :min="0" :precision="2" style="width: 150px;"/>
+                <el-select v-model="materialInfo.company" filterable allow-create :reserve-keyword="false" placeholder="单位" style="width: 80px;">
                   <el-option v-for="item in materialInfo.companyOptions" :key="item.value" :label="item.label" :value="item.value" />
                 </el-select>
                 <el-button type="primary" :icon="Plus" circle @click="plusMaterial"/>
                 <el-button type="primary" :icon="Minus" circle :disabled="materialInfo.materialList.length === 0" @click="minusMaterial"/>
               </div>
-              <el-input v-model="materialInfo.materials" disabled autosize type="textarea" />
+              <el-input v-model="materialInfo.materials" disabled :rows="8" type="textarea" />
             </el-form-item>
             <el-form-item label="注意事项">
-              <el-input v-model="cookeryBookInfo.note" autosize type="textarea" />
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="onSubmit">确定</el-button>
-              <el-button>取消</el-button>
+              <el-input v-model="cookeryBookInfo.note" :rows="5" type="textarea" />
             </el-form-item>
           </el-form>
         </el-col>
         <el-col :span="12">
-          <h1>具体步骤</h1>
+          <div class="title-div">
+            <el-tag class="ml-2" type="info" size="large" round>具体步骤</el-tag>
+          </div>
           <div class="new-step-div">
             <el-input v-model="stepInfo.newStep" autosize type="textarea" placeholder="请添加步骤" style="width: 70%; margin-right: 5px;" />
             <el-button type="primary" :icon="Plus" circle @click="plusStep"/>
             <el-button type="primary" :icon="Minus" circle :disabled="stepInfo.stepList.length === 0" @click="minusStep"/>
           </div>
 
-          <el-steps direction="vertical" align-center style="height: 500px;">
+          <el-steps direction="vertical" align-center style="height: 490px;">
             <!-- 使用模板字符串拼接 -->
             <el-step
               v-show="stepInfo.stepList.length > 0"
@@ -69,6 +69,11 @@
           </el-steps>
         </el-col>
       </el-row>
+
+      <div style="text-align: center;">
+        <el-button type="primary" @click="onSubmit">确定</el-button>
+        <el-button>取消</el-button>
+      </div>
 
       <button class="back-button" @click="backToIndex">
         <svg t="1663131339342" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4852" width="32" height="32"><path d="M675.328 117.717333A425.429333 425.429333 0 0 0 512 85.333333C276.352 85.333333 85.333333 276.352 85.333333 512s191.018667 426.666667 426.666667 426.666667 426.666667-191.018667 426.666667-426.666667c0-56.746667-11.093333-112-32.384-163.328a21.333333 21.333333 0 0 0-39.402667 16.341333A382.762667 382.762667 0 0 1 896 512c0 212.074667-171.925333 384-384 384S128 724.074667 128 512 299.925333 128 512 128c51.114667 0 100.8 9.984 146.986667 29.12a21.333333 21.333333 0 0 0 16.341333-39.402667z m-324.693333 373.013334l174.464-174.485334a21.141333 21.141333 0 0 0-0.192-29.973333 21.141333 21.141333 0 0 0-29.973334-0.192l-208.256 208.256a20.821333 20.821333 0 0 0-6.122666 14.976c0.042667 5.418667 2.133333 10.837333 6.314666 14.997333l211.178667 211.2a21.141333 21.141333 0 0 0 29.973333 0.213334 21.141333 21.141333 0 0 0-0.213333-29.973334l-172.629333-172.629333 374.997333 2.602667a20.693333 20.693333 0 0 0 21.034667-21.034667 21.482667 21.482667 0 0 0-21.333334-21.333333l-379.242666-2.624z" fill="#3D3D3D" p-id="4853"></path></svg>
@@ -204,9 +209,9 @@ const backToIndex = () => {
       min-height: 36px;
     }
 
-    h1 {
+    .el-tag {
       font-size: 24px;
-      text-align: center;
+      color: black;
     }
 
     .el-button, .el-input, .el-input-number, .el-select {
@@ -224,6 +229,11 @@ const backToIndex = () => {
 
 <style lang="scss" scoped>
 .cookery-book-detail {
+  .title-div {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
   .new-step-div {
     margin-bottom: 10px;
   }
--- src/views/travel/index.vue
+++ src/views/travel/index.vue
@@ -1,34 +1,75 @@
 <template>
-  <el-table :data="tableData" border stripe style="width: 100%">
-    <el-table-column type="index" align="center" width="50" />
-    <el-table-column prop="date" label="景点名称" align="center" width="180"/>
-    <el-table-column prop="name" label="Name" align="center" width="180" />
-    <el-table-column prop="address" label="Address" align="center" />
-  </el-table>
+  <div>
+    <div style="text-align: center;">
+      <el-button type="primary" @click="yearCardDialogVisible = true">年卡使用情况</el-button>
+    </div>
+
+    <el-timeline>
+      <el-timeline-item timestamp="2018/4/12" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/12 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/3" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/3 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/2" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/2 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/2" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/2 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/2" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/2 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/2" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/2 20:46</p>
+        </el-card>
+      </el-timeline-item>
+    </el-timeline>
+
+    <el-dialog v-model="yearCardDialogVisible" :show-close="false">
+      <template #header="{ close, titleId, titleClass }">
+        <div class="my-header">
+          <h4 :id="titleId" :class="titleClass">This is a custom header!</h4>
+          <el-button type="danger" @click="close">
+            <el-icon class="el-icon--left"><CircleCloseFilled /></el-icon>
+            Close
+          </el-button>
+        </div>
+      </template>
+      This is dialog content.
+    </el-dialog>
+  </div>
 </template>
 
 <script lang="ts" setup>
-const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles'
-  }
-]
+import { ref } from 'vue'
+import { ElButton, ElDialog } from 'element-plus'
+import { CircleCloseFilled } from '@element-plus/icons-vue'
+
+const yearCardDialogVisible = ref(false)
 </script>
 
+<style lang="scss" scoped>
+.my-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
